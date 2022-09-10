8a5de985e88c8675de098363c111dee66f71e333
Nagbhushan <nagbhushanbhalerao358@gmail.com>
2022-09-10 22:24:00 +0530

    main commit

---
+++ src/main/java/com/demo/demo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("m1");
+        System.out.println("m2");
+        System.out.println("m3");
+	}
+
+}
